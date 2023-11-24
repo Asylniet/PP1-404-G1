5731e297c42a46a7454352c0c36367d92f7a4a4b
Asylniet <zasylniet@mail.ru>
2023-11-24 21:33:48 +0600

    GOMU GOMU NO

--- 10practice/bb.cpp
+++ 10practice/bb.cpp
@@ -1,28 +1,48 @@
 // IGNORE FOR NOW WILL PUSH WORKING CODE LATER
 // 6^&678JKjdkdl;?.,lk879Pk1kdfl4839
 #include <iostream>
-#include <string>
 using namespace std;
 
-string toBinary(string s) {
-  int number = std::stoi(s);
-  if (number == 0) return "";
-  char digit = number % 2 + '0';
-  return toBinary(number / 2) + digit;
+string convertToBinary(int number) {
+  string binary;
+  while (number > 0) {
+      binary = (char)('0' + (number % 2)) + binary;
+      number /= 2;
+  }
+  return binary.empty() ? "0" : binary;
+}
+
+string replaceNumbersWithBinary(const string& input) {
+  string result;
+  string currentNumber;
+
+  for (int i = 0; i < input.size(); i++) {
+    char ch = input[i];
+    if(isdigit(ch)) {
+      currentNumber += ch;
+    } else {
+      if (!currentNumber.empty()) {
+        int number = stoi(currentNumber);
+        string binary = convertToBinary(number);
+        result += binary;
+        currentNumber.clear();
+      }
+      result += ch;
+    }
+  }
+
+  if (!currentNumber.empty()) {
+    int number = stoi(currentNumber);
+    string binary = convertToBinary(number);
+    result += binary;
+  }
+
+  return result;
 }
 
 int main() {
-  string s;
-  cin >> s;
-  for(int i = 0; i < s.size(); i++) {
-    string number = "";
-    if(isdigit(s[i])) {
-      number += s[i];
-    } else {
-      if(number != "") {
-        cout << toBinary(number);
-      }
-    }
-  }
+  string input = "6^&678JKjdkdl;?.,lk879Pk1kdfl4839";
+  string output = replaceNumbersWithBinary(input);
+  cout << output;
   return 0;
 }
